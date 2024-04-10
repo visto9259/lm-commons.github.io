37b867f6a4d7a358b5f6b95d8909a9f3c019c8de
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2024-04-10 10:47:05 -0400

    Updated prism configuration to support bash, json and php  languages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -179,6 +179,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ['bash', 'json', 'php']
       },
     }),
 };
